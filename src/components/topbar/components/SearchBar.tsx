5948e83bdb532cbd153ffeb2100331d6116726fb
17g01acs028 <stevemutioo123@gmail.com>
2024-04-14 08:57:58 +0300

    latest commit

--- src/components/topbar/components/SearchBar.tsx
+++ src/components/topbar/components/SearchBar.tsx
@@ -8,7 +8,7 @@ const SearchBar = () => {
                 <div className="relative  flex gap-6 items-center justify-center md:justify-normal">
                     <div className="flex items-center gap-6">
                         <HomeIcon className="left-2.5 hidden md:flex  h-4 w-4 text-muted-foreground" />
-                        <span>Welcome to goal management system</span> 
+                        <span className="hidden md:block">Welcome to goal management system</span> 
                     </div>
                 </div>
             </form>
